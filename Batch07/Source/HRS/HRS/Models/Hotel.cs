093bb40f50942b25e29bf88f54c089dc3d89c4c6
Mahedee Hasan (মেহেদী) <mahedee.hasan@gmail.com; mahedee.hasan@hotmail.com>
2017-01-10 20:41:15 +0600

    initial upload

---
+++ Batch07/Source/HRS/HRS/Models/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HRS.Models
+{
+    public class Hotel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(250)]
+        public string Name { get; set; }
+    }
+}
