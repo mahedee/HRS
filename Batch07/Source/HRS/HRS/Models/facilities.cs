3fdf09d231d486544b13087f7aa22d9b800355be
Faiyaz Ahmed <raaz_99@yahoo.com>
2017-01-11 19:40:11 +0600

    commit by razz-001

---
+++ Batch07/Source/HRS/HRS/Models/facilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HRS.Models
+{
+    public class facilities
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        //[Required (ErrorMessage = "Must be Description")]
+        public string Description { get; set; }
+    }
+}
