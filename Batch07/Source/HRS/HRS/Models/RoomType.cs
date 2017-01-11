4200446ed485dfe3dbb48fb899a5b0c522aeffa8
apuleads <golammamun80@gmail.com>
2017-01-11 19:25:27 +0600

    commit by apu
    added room type

---
+++ Batch07/Source/HRS/HRS/Models/RoomType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HRS.Models
+{
+    public class RoomType
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(250, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int GaleryId { get; set; }
+        public string ImageURL { get; set; }
+        [Required(ErrorMessage = "Price must Required")]
+        public double Price { get; set; }
+
+    }
+}
