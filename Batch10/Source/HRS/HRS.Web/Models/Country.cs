fe324f992a433e2765b263fc0dfe6bdd9dbe8ced
mahedee <mahedee.hasan@gmail.com>
2017-08-16 20:09:45 +0600

    added for batch 10

---
+++ Batch10/Source/HRS/HRS.Web/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HRS.Web.Models
+{
+    public class Country
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Country Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Batch10/Source/HRS/HRS.Web/Models/LocationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRS.Web.Models
+{
+    public class LocationInfo
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public int CityId { get; set; }
+        public string LocationName { get; set; }
+        public string LocationHistory { get; set; }
+        public string HowToGo { get; set; }
+        public string Image { get; set; }
+    }
+}
